5bdc1834f624ca40a79e60d1587f6f0807ad1336
Taras Kosiachenko <t.kosiachenko@gmail.com>
2020-06-14 17:16:09 +0300

    calculator

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ function insert(num) {
     document.form.textview.value = 
     document.form.textview.value + num; 
 }
+
 function clean () {
     document.form.textview.value = "";
 }
+
 function erase () {
     let exp = document.form.textview.value;
     document.form.textview.value = exp.substring(0,exp.length-1);
 }
+
 function count () {
     let exp = document.form.textview.value;
     if(exp) {
